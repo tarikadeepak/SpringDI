5b18889dd9f0d4b88f019f184778ead966ee7d2d
tarikadeepak <tarikadeepak@gmail.com>
2019-06-23 05:40:39 +0530

    added property in the xml

--- src/main/java/com/spring/framework/dt/Triangle.java
+++ src/main/java/com/spring/framework/dt/Triangle.java
@@ -2,9 +2,16 @@ package com.spring.framework.dt;
 
 public class Triangle {
 	private String type; 
+	private String msg;
 	
 	public String getType() {
 		return type;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
 	}
 	public void setType(String type) {
 		this.type = type;
@@ -12,6 +19,7 @@ public class Triangle {
 	
 	public void draw() {
 		System.out.println("Drawing Triangle " + getType());
+		System.out.println("Printing Message from Spring.XML " + getMsg());
 	}
 	
 	public void print() {
